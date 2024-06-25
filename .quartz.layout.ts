6ab9991072836a501bf652a6527690c6c0e8aebd
chicoarts <chicoconsuarte@gmail.com>
2024-06-25 10:38:29 -0300

    Quartz sync: Jun 25, 2024, 10:38 AM

--- .quartz.layout.ts
+++ .quartz.layout.ts
@@ -43,7 +43,15 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+    	Component.Explorer({
+	  filterFn: (node) => {
+	    // set containing names of everything you want to filter out
+	    const omit = new Set(["es"])
+	    return !omit.has(node.name.toLowerCase())
+	  },
+	})
+  ),
   ],
   right: [],
 }
